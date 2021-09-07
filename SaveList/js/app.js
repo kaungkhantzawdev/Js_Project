dd9d973027c875082b38b42b9d0e08bee834e48b
Channyeinaung885-source <kaungkhantzaw2001326@gmail.com>
2021-09-07 12:52:56 +0630

    update_list

--- SaveList/js/app.js
+++ SaveList/js/app.js
@@ -2,49 +2,77 @@ let data = document.getElementById("data");
 let saveList = document.getElementById("data_btn");
 let showData = document.getElementById("show_data");
 
-let update = document.getElementById("update");
-let del = document.getElementById("del");
+let i = 1;
 
 saveList.addEventListener("click", function (){
-    let currentData = data.value;
-    console.log(currentData);
-    showData.innerHTML = `
-          <tr>
-                   <td>${currentData}</td>
-                   <td><button class="btn-sm primary" id="update" onclick="upDate()">Update</button></td>
-                   <td><button class="btn-sm danger" id="del" onclick="Del()">Delete</button></td>
-          </tr>
-    `;
+    // console.log(currentData);
+    checkData(data.value);
+    data.value = "";
+
 })
 
-function upDate(){
-   let x = prompt("Write Data");
-    showData.innerHTML = `
-          <tr>
-                   <td>${x}</td>
-                   <td><button class="btn-sm primary" id="update" onclick="upDate()">Update</button></td>
-                   <td><button class="btn-sm danger" id="del">Delete</button></td>
-          </tr>
-    `;
+function checkData(data){
+    if (data){
+        if (data.length >= 3){
+           return  dataShow(data);
+        }else {
+            console.log("Minimum number must be three words.");
+            return  alert("Minimum number must be three words.");
+        }
+    }else {
+        console.log("Please fill data");
+        return   alert("Please fill data");
+    }
 }
 
-function Del(){
-    let x = window.confirm("Are you sure");
-    console.log(x);
+function upDate(d){
+    let current = document.getElementById(`dataList${d}`).cells[1].innerHTML;
+    // console.log(currentData);
+    let data = prompt("Please change data", current);
+     if(data){
+         if (data.length >= 3){
+             return document.getElementById(`dataList${d}`).cells[1].innerHTML = data ;
+         }else {
+             console.log("Minimum number must be three words.");
+             return  alert("Minimum number must be three words.");
+         }
+     }else {
+         console.log("Please fill data");
+         return   alert("Please fill data");
+     }
 
-    if (x === true){
-        showData.innerHTML = "";
-    }else {
-        let currentData = data.value;
-        console.log(currentData);
-        showData.innerHTML = `
-          <tr>
+}
+
+function dataShow(currentData){
+    showData.innerHTML += `
+          <tr id="dataList${i}">
+                   <td>${i}</td>
                    <td>${currentData}</td>
-                   <td><button class="btn-sm primary" id="update" onclick="upDate()">Update</button></td>
-                   <td><button class="btn-sm danger" id="del" onclick="Del()">Delete</button></td>
+                   <td><button class="btn-sm primary" id="update${i}" onclick="upDate(${i})">Update</button></td>
+                   <td><button class="btn-sm danger" id="del${i}" onclick="Del(${i})">Delete</button></td>
           </tr>
     `;
+    i++;
+}
+
+
+function Del(current){
+    let x = document.getElementById(`dataList${current}`);
+
+    let one = confirm(`Are you sure to delete "${x.cells[1].innerHTML}" `);
+    if (one){
+        showData.removeChild(x);
     }
 
 }
+
+
+data.addEventListener("keypress", function (e){
+    if(e.keyCode === 13){
+        checkData(data.value);
+        data.value = "";
+    }
+})
+
+
 
